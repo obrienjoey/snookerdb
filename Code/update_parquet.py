a3c6d0adc10699b3cde2ace974d5c064bc3f0eb6
obrienjoey <mr.joeyob@gmail.com>
2022-08-03 23:47:13 +0100

    corrected file location parquet

--- Code/update_parquet.py
+++ Code/update_parquet.py
@@ -12,7 +12,7 @@ def sql_to_pq(table, conn):
     """
     df = pd.read_sql_query(f"SELECT * from {table}", conn)
     pq_table = pa.Table.from_pandas(df)
-    pq.write_table(pq_table, f'../Parquet/{table}.parquet')
+    pq.write_table(pq_table, f'Parquet/{table}.parquet')
 
 tables = ['players', 'tournament', 'matches']
 for table in tables:
